2da97c0be90047a9b71330b2e3c301aa7c49e73c
happy-tony <60281710+Serdyuk-prog@users.noreply.github.com>
2021-10-11 15:13:37 +0700

    done

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-const button = document.querySelector("#search-button");
 const countDiv = document.querySelector("#count");
 
 const searchCells = Array.from(document.querySelectorAll("[data-label='Сountry']"));
 
 const input = document.querySelector("#search-input");
 
-const searchArray = searchCells.map((el) => {
-    return el.innerText.trim();
-});
-
-const buttonClickHandler = () => {
+const submitSearchHandler = () => {
     if (input.value.trim().length != 0) {
         const matches = getMatches(input.value);
         if (matches.length > 0) {
             countDiv.innerText = `Number of matches: ${matches.length}!`
-            transformMatches(matches, input.value);
+            transformCells(searchCells, input.value);
         } else {
             countDiv.innerText = "Nothing found"
         }
         countDiv.style.display = "block";
     } else {
         countDiv.style.display = "none";
+        transformCells(searchCells, "");
     }
 }
 
@@ -29,18 +25,23 @@ const getMatches = (string) => {
     return result;
 }
 
-const transformMatches = (matches, search) => {
+const transformCells = (matches, search) => {
     for (el of matches) {
         el.innerHTML = el.querySelector("img").outerHTML + " " + highlightedString(el.innerText.trim(), search);
     }
 }
 
-button.addEventListener("click", buttonClickHandler)
-
-
 function highlightedString(name, search) {
-    let string = search;
+    let string = search.toLowerCase();
+    if (name.toLowerCase().indexOf(string) == 0) {
+        string = string.charAt(0).toUpperCase() + string.slice(1);
+    }
     const regex = new RegExp(string, 'i')
     const newName = name.replace(regex, `<mark>${string}</mark>`);
     return `<span>${newName}</span>`
 }
+
+document.querySelector("#search").addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitSearchHandler();
+});
